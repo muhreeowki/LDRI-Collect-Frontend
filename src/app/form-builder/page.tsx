d0ad38404a5cee8ae55384d14781ba5bd8818190
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2025-06-27 17:38:09 +0300

    Added custom form builder into frontend.

---
+++ src/app/form-builder/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FormBuilderMain } from '@/form-builder/components/form-builder-main';
+
+const FormBuilder = () => {
+  return (
+    <div>
+      <FormBuilderMain />
+    </div>
+  );
+};
+
+export default FormBuilder;
